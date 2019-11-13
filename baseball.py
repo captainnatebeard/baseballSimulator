c4b9a2b20d7c78e952d4524fe627f6abc866ead5
Nathan Johnson <nate@Nathans-MacBook-Pro-2.local>
2019-11-12 20:16:26 -0800

    first commit

---
+++ baseball.py
@@ -0,0 +1,115 @@
+import random
+
+class Team:
+    def __init__(self, lineup, pitchers, name):
+        self.lineup = lineup
+        self.pitchers = pitchers
+        self.name = name
+        #assert type(lineup) == type([]), "lineup must be a list... you jive turkey"
+
+    def __repr__(self):
+        return "%s" % self.name
+
+
+class Pitcher:
+    def __init__(self, bAvgAgainst, name, handed):
+        self.bAvgAgainst = bAvgAgainst
+        self.name = name
+        self.handed = handed
+
+
+    def __repr__(self):
+        return "%s" % self.name
+
+
+
+class Player:
+    def __init__(self, bavg, name, statusList, handed):
+        self.bavg = bavg
+        self.name = name
+        self.statusList = statusList
+        self.handed = handed
+
+
+    def __repr__(self):
+        return "%s" % self.name
+
+
+    def status(self, pitcher):
+        t = random.randrange(1, 1001)
+        s = ((self.bavg * 1000) * (pitcher.bAvgAgainst * 1000)) / 324
+        print ("batting average:", self.bavg)
+        print ("pitcher batting average against:", pitcher.bAvgAgainst)
+        print ("randomly picked number", t)
+        print ("combined average:", s)
+        if t < s:
+            #singles - 66%
+            #doubles - 20%
+            #triples - 11%
+            #homeruns - 3%
+            hitrand = random.randrange(1, 101)
+            if hitrand > 97:
+                playerStatus = 4
+            elif (hitrand > 86) & (hitrand <= 97):
+                playerStatus = 3
+            elif (hitrand > 66) & (hitrand <= 77):
+                playerStatus = 2
+            else:
+                playerStatus = 1
+        else:
+            playerStatus = 0
+        return playerStatus
+
+
+jMccann = Player(.318, "James McCann", [], "right")
+mCabrera = Player(.329, "Miguel Cabrera", [], "right")
+iKingsler = Player(.313, "Ian Kingsler", [], "right")
+jIglesias = Player(.288, "Jose Iglesias", [], "right")
+nCastellanos = Player(.320, "Nicholas Castellanos", [], "right")
+jUpton = Player(.362, "Justin Upton", [], "right")
+mMahtook = Player(.330, "Mikie Mahtook", [], "right")
+jMartinez = Player(.388, "J.D. Martinez", [], "right")
+vMartinez = Player (.324, "Victor Martinez", [], "right")
+
+jVerlander = Pitcher(.306, "Justin Verlander", "right")
+mFulmer = Pitcher(.293, "Michael Fulmer", "right")
+jZimmerman = Pitcher(.358, "Jordan Zimmerman", "right")
+mBoyd = Pitcher(.352, "Matt Boyd", "right")
+aSanchez = Pitcher(.357, "Anibal Sanchez", "right")
+dNorris = Pitcher(.363, "Daniel Norris", "right")
+jWilson = Pitcher(.242, "Justin Wilson", "right")
+sGreene = Pitcher(.311, "Shane Greene", "right")
+wSaupold = Pitcher(.361, "Warwick Saupold", "right")
+aWilson = Pitcher(.327, "Alex Wilson", "right")
+dStumpf = Pitcher(.338, "Daniel Stumpf", "right")
+
+tigers = Team([jMccann, mCabrera, iKingsler, jIglesias, nCastellanos, jUpton, mMahtook, jMartinez,
+			vMartinez],[jVerlander, mFulmer, jZimmerman, mBoyd, aSanchez, dNorris, jWilson,
+			sGreene, wSaupold, aWilson, dStumpf], "Detroit Tigers")
+
+
+mZunino = Player(.331, "Mike Zunino", [], "right")
+dValencia = Player(.314, "Danny Valencio", [], "right")
+rCano = Player(.338, "Robinson Cano", [], "right")
+jSegura = Player(.349, "Jean Segura", [], "right")
+kSeager = Player(.323, "Kyle Seager", [], "right")
+bGamel = Player(.322, "Ben Gamel", [], "right")
+jDyson = Player(.324, "Jarrod Dyson", [], "right")
+mHaniger = Player(.352, "Mitch Haniger", [], "right")
+nCruz = Player(.375, "Nelson Cruz", [], "right")
+
+aMiranda = Pitcher(.307, "Ariel Miranda", "right")
+jPaxton = Pitcher(.277, "James Paxton", "right")
+yGallardo = Pitcher(.346, "Yovani Gallardo", "right")
+fHernandez = Pitcher(.321, "Felix Hernandez", "right")
+sGaviglio = Pitcher(.332, "Sam Gaviglio", "right")
+eRamirez = Pitcher(.284, "Erasmo Ramirez", "right")
+eDiaz = Pitcher(.284, "Edwin Diaz", "right")
+nVincent = Pitcher(.286, "Nick Vincent", "right")
+jPazos = Pitcher(.333, "James Pazos", "right")
+tZych = Pitcher(.323, "Tony Zych", "right")
+mRzepczynski = Pitcher(.365, "Marc Rzepczynski", "right")
+
+mariners = Team([mZunino, dValencia, rCano, jSegura, kSeager, bGamel, jDyson, mHaniger, nCruz],
+            [aMiranda, jPaxton, yGallardo, fHernandez, sGaviglio, eRamirez, eDiaz, nVincent,
+             jPazos, tZych, mRzepczynski],"Seattle Mariners")
---
+++ baseballSimulator.py
@@ -0,0 +1,131 @@
+#baseballsim.py
+#This program simulates n number of baseball games between two MLB baseball teams inputted by the user
+#and then outputs the odds that either team will win
+
+from baseball import *
+
+
+def main():
+    n, i, team1, team2, sPitcher1, rPitcher1, sPitcher2, rPitcher2 = intro()
+    wincount1 = 0
+    wincount2 = 0
+    for i in range(n):
+        print("\n")
+        print("game number:", i + 1, "\n")
+        score = oneGame(team1, team2, sPitcher1, rPitcher1, sPitcher2, rPitcher2)
+        wincount1, wincount2 = results(n, score, team1, team2, wincount1, wincount2)
+
+def intro():
+    team1 = eval(input("Enter the away team: "))
+    for i in range (len(team1.pitchers)):
+        print ("(", i + 1, ") ", team1.pitchers[i], sep = "")
+    sPitcher1 = eval(input("Enter starting pitcher (pick the corresponding number): "))
+    rPitcher1 = eval(input("Enter relief pitcher (pick the corresponding number): "))
+    team2 = eval(input("Enter the home team: "))
+    for i in range (len(team2.pitchers)):
+        print ("(", i + 1, ")", team2.pitchers[i], sep = "")
+    sPitcher2 = eval(input("Enter starting pitcher (pick the corresponding number): "))
+    rPitcher2 = eval(input("Enter relief pitcher (pick the corresponding number): "))
+    n = int(input("how many games: "))
+    return n, i, team1, team2, sPitcher1, rPitcher1, sPitcher2, rPitcher2
+
+def atBat(batter, startingPitcher, reliefPitcher, inning):
+    print ("batter: ", batter)
+    if inning < 8:
+        print ("pitcher:", startingPitcher)
+        p = Player.status(batter, startingPitcher)
+    else:
+        print ("pitcher:", reliefPitcher)
+        p = Player.status(batter, reliefPitcher)
+    return p
+
+
+def oneGame(team1, team2, sPitcher1, rPitcher1, sPitcher2, rPitcher2):
+    inning = 1
+    outs = 0
+    score = [0,0]
+    game = True
+    ondeck = [0, 0]
+    bases1 = [False, False, False, False, False]
+    bases2 = [False, False, False, False, False]
+    teams = [team1, team2]
+    bases = [bases1, bases2]
+    sPitcher = [sPitcher1, sPitcher2]
+    rPitcher = [rPitcher1, rPitcher2]
+
+    while game:
+        if inning % 1 == 0:
+            ups = 0
+            downs = 1
+        else:
+            ups = 1
+            downs = 0
+        x = atBat(teams[ups].lineup[ondeck[ups]], teams[downs].pitchers[sPitcher[downs]-1], teams[downs].pitchers[rPitcher[downs]-1], inning)
+        teams[ups].lineup[ondeck[ups]].statusList.append(x)
+        if teams[ups].lineup[ondeck[ups]].statusList[-1] == 0:
+            print ("Out")
+        if teams[ups].lineup[ondeck[ups]].statusList[-1] == 1:
+            print ("Single")
+        if teams[ups].lineup[ondeck[ups]].statusList[-1] == 2:
+            print ("Double")
+        if teams[ups].lineup[ondeck[ups]].statusList[-1] == 3:
+            print ("Triple")
+        if teams[ups].lineup[ondeck[ups]].statusList[-1] == 4:
+            print ("HOME RUN!")
+        for i in range(3, 0, -1):
+            if bases[ups][i]:
+                bases[ups][i] = False
+                if (i + x) < 4:
+                    bases[ups][i + x] = True
+                else:
+                    score[ups] += 1
+        bases[ups][x] = True
+        ondeck[ups] += 1
+        if bases[ups][0]:
+            outs += 1
+            bases[ups][0] = False
+        if bases[ups][4]:
+            score[0] += 1
+
+        print(teams[ups])
+        print("inning:", inning, " outs:", outs, " score:", score, sep="")
+        print(bases[ups])
+        bases[ups][4] = False
+        print()
+
+        if outs == 3:
+            inning += .5
+            bases[ups] = [False, False, False, False, False]
+            outs = 0
+        if ondeck[ups] == len(teams[ups].lineup):
+            ondeck[ups] = 0
+        if inning >= 10 and score[0] != score[1] and inning % 1 == 0:
+            game = False
+    return score
+
+def results(n, score, team1, team2, wincount1, wincount2):
+    print("Final Score", score)
+    if float(score[0]) > float(score[1]):
+        print (team1, "win!")
+        wincount1 += 1
+    if float(score[1]) > float(score[0]):
+        print (team2, "win!")
+        wincount2 += 1
+    if n == (wincount1 + wincount2):
+        print ()
+        print (team1, ":", wincount1, sep = "")
+        print (team2, ":", wincount2, sep = "")
+        percentagewon1 = (wincount1/(wincount1 + wincount2)) * 100
+        percentagewon2 = (wincount2/(wincount1 + wincount2)) * 100
+        print (team1, " have a ", percentagewon1,"%", " chance of winning", sep = "")
+        print (team2, " have a ", percentagewon2,"%", " chance of winning", sep = "")
+    return wincount1, wincount2
+
+if __name__ == '__main__':
+    main()
+
+"""
+TODO:
+-add ways for players on base to get thrown out
+-add graphics
+"""
